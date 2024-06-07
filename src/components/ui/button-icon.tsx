385e9caf0a2d7dc3f17250a77b7a47307725e9ce
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-07 15:55:43 +0200

    refactor: Remove unused Link import and add hover effect to ButtonIcon component

--- src/components/ui/button-icon.tsx
+++ src/components/ui/button-icon.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Link from 'next/link';
 import { Button } from './button';
 import Image from 'next/image';
 
@@ -9,17 +8,15 @@ export interface ButtonIconProps
 const ButtonIcon = React.forwardRef<HTMLButtonElement, ButtonIconProps>(
   ({ children }) => {
     return (
-      <Link className="hidden md:inline-flex" href="/login">
-        <Button variant={'ghost'}>
-          {children}
-          <Image
-            src={'/icons/icon-arrow-right.svg'}
-            alt="icon arrow right"
-            width={8}
-            height={16}
-          />
-        </Button>
-      </Link>
+      <Button variant={'ghost'} className="group-hover:text-primary">
+        {children}
+        <Image
+          src={'/icons/icon-arrow-right.svg'}
+          alt="icon arrow right"
+          width={8}
+          height={16}
+        />
+      </Button>
     );
   },
 );
