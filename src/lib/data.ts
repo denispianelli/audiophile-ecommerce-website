52855ac49298134d74c114e6261baa12609f45b0
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-12 22:46:25 +0200

    refactor: Add lazy loading for images and improve component organization in data.ts

---
+++ src/lib/data.ts
@@ -0,0 +1,70 @@
+import db from '@/db/prisma';
+
+export async function getAllProducts() {
+  try {
+    const products = await db.product.findMany({
+      include: {
+        categoryImage: true,
+      },
+    });
+
+    return products;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error getting products');
+  }
+}
+
+export async function getProductsByCategory(category: string) {
+  try {
+    const products = await db.product.findMany({
+      orderBy: {
+        id: 'desc',
+      },
+      include: {
+        categoryImage: true,
+      },
+      where: {
+        category: category,
+      },
+    });
+
+    return products;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error getting products by category');
+  }
+}
+
+export async function getProductBySlug(slug: string) {
+  try {
+    const product = await db.product.findUnique({
+      where: {
+        slug: slug,
+      },
+      include: {
+        image: true,
+        GalleryImage: true,
+        other_products: {
+          select: {
+            other_product: {
+              include: {
+                image: true,
+              },
+            },
+          },
+        },
+        item_includes: {
+          select: {
+            item_includes: true,
+          },
+        },
+      },
+    });
+
+    return product;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error getting product by name');
+  }
+}
