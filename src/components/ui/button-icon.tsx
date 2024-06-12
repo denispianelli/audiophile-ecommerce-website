c3bec44ca728bf6ff1431e5ef707e41baa6621d3
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-12 22:46:10 +0200

    refactor: Update ButtonIcon component to include lazy loading for images and improve component organization

--- src/components/ui/button-icon.tsx
+++ src/components/ui/button-icon.tsx
@@ -1,20 +1,24 @@
 import * as React from 'react';
 import { Button } from './button';
 import Image from 'next/image';
+import arrowRight from '../../../public/icons/icon-arrow-right.svg';
 
 export interface ButtonIconProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 const ButtonIcon = React.forwardRef<HTMLButtonElement, ButtonIconProps>(
-  ({ children }) => {
+  ({ children }, ref) => {
     return (
-      <Button variant={'ghost'} className="group-hover:text-primary">
+      <Button
+        ref={ref}
+        variant={'ghost'}
+        className="subtitle group-hover:text-primary"
+      >
         {children}
         <Image
-          src={'/icons/icon-arrow-right.svg'}
+          src={arrowRight}
           alt="icon arrow right"
-          width={8}
-          height={16}
+          style={{ width: '8px', height: 'auto' }}
         />
       </Button>
     );
