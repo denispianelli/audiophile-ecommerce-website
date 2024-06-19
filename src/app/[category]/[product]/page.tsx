8e20d0a2006c6421b8f4132642545f94bcd9b545
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-19 10:20:06 +0200

    refactor: Update metadata generation for category page

--- src/app/[category]/[product]/page.tsx
+++ src/app/[category]/[product]/page.tsx
@@ -2,6 +2,36 @@ import { getProductBySlug } from '@/lib/data';
 import Product from './_components/product';
 import { notFound } from 'next/navigation';
 
+import { Metadata } from 'next';
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { category: string; product: string };
+}): Promise<Metadata> => {
+  const { category, product } = params;
+  const productData = await getProductBySlug(product);
+  const VALID_CATEGORIES = ['headphones', 'speakers', 'earphones'];
+
+  if (!VALID_CATEGORIES.includes(category)) {
+    return {
+      title: 'Page Not Found',
+      description: 'The page you are looking for does not exist.',
+    };
+  }
+
+  if (!productData) {
+    return {
+      title: 'Page not found',
+      description: 'The page you are looking for does not exist.',
+    };
+  }
+
+  return {
+    title: `${product}`,
+  };
+};
+
 export default async function Page({
   params,
 }: {
@@ -9,9 +39,11 @@ export default async function Page({
 }) {
   const { category, product } = params;
   const productData = await getProductBySlug(product);
+  console.log('productData:', productData);
   const VALID_CATEGORIES = ['headphones', 'speakers', 'earphones'];
 
   if (!VALID_CATEGORIES.includes(category)) return notFound();
+  if (!productData) return notFound();
 
   return <Product product={productData} />;
 }
