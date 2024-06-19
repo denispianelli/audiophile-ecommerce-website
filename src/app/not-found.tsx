b5b4b6e016964c4f5a32890165cfd12e0bf04fe5
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-19 10:19:42 +0200

    refactor: Update metadata title template and description

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,11 @@
 import { Header } from '@/components/header/page-header';
 import { Separator } from '@/components/ui/separator';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Page Not Found',
+  description: 'The page you are looking for does not exist.',
+};
 
 export default function NotFound() {
   return (
