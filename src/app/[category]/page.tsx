b8fe150be84037be1aafe8ce8aa3b32240108c04
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-19 10:20:02 +0200

    refactor: Update metadata generation for category page

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,5 +1,26 @@
 import { notFound } from 'next/navigation';
 import Category from './_components/category';
+import { Metadata } from 'next';
+
+export const generateMetadata = ({
+  params,
+}: {
+  params: { category: string };
+}): Metadata => {
+  const { category } = params;
+  const VALID_CATEGORIES = ['headphones', 'speakers', 'earphones'];
+
+  if (!VALID_CATEGORIES.includes(category)) {
+    return {
+      title: 'Page not found',
+      description: 'The page you are looking for does not exist.',
+    };
+  }
+
+  return {
+    title: `${category}`,
+  };
+};
 
 export default function Page({ params }: { params: { category: string } }) {
   const { category } = params;
